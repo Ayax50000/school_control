2c58eb5f3e7c7806f274611bf66abfe26cf0dd43
Ayax50000 <fjlsoto@gmail.com>
2013-12-22 20:38:28 -0600

    issues in the main data structura fixed

--- SchoolControl.rb
+++ SchoolControl.rb
@@ -1,13 +1,12 @@
 class School
     def initialize
-        @control={:Fisica=>{:Maestros=>["Juan Carlos Hernandez","Juan Perez"],
-                            :Alumnos =>[{:Alumno=>"",:Calificacion= 0.0}] },
-                  :Matematicas=>{:Maestros=>["Olivia Martinez", "Juan Perez"],
-                            :Alumnos =>[{:Alumno=>"",:Calificacion= 0.0}] },
-                  :BasesDeDatos=>{:Maestros=>["Olivia Martinez"],
-                            :Alumnos =>[{:Alumno=>"",:Calificacion= 0.0}] },}
+        @control ={:Fisica=> {:Maestros=>["Juan Carlos Hernandez","Juan Perez"], :Alumnos=>[{:Alumno=>"",:Calificacion=>0.0}] },
+                   :Matematicas=>{:Maestros=>["Olivia Martinez", "Juan Perez"], :Alumnos =>[{:Alumno=>"",:Calificacion=>0.0}] },
+                  :BasesDeDatos=>{:Maestros=>["Olivia Martinez"], :Alumnos =>[{:Alumno=>"",:Calificacion=>0.0}] }}
     end
-    def mostrar
-        puts "los datos que ahy son #{@control}"
+    def ver_materias
+        @control.each_key do |llave|
+        puts "los datos que ahy son #{llave}"
+        end
     end
 end
