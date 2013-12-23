6eda47344d5f15b8d4d335ea911be42934b59a78
Ayax50000 <fjlsoto@gmail.com>
2013-12-22 21:34:59 -0600

    method to see students done

--- SchoolControl.rb
+++ SchoolControl.rb
@@ -1,8 +1,8 @@
 class Escuela
     def initialize
-        @control ={:Fisica=> {:Maestros=>["Juan Carlos Hernandez","Juan Perez"], :Alumnos=>[{:Alumno=>"",:Calificacion=>0.0}] },
-                   :Matematicas=>{:Maestros=>["Olivia Martinez", "Juan Perez"], :Alumnos =>[{:Alumno=>"",:Calificacion=>0.0}] },
-                  :BasesDeDatos=>{:Maestros=>["Olivia Martinez"], :Alumnos =>[{:Alumno=>"",:Calificacion=>0.0}] }}
+        @control ={:Fisica=> {:Maestros=>["Juan Carlos Hernandez","Juan Perez"], :Alumnos=>[{:Nombre=>"Juan",:Calificacion=>0.0}] },
+                   :Matematicas=>{:Maestros=>["Olivia Martinez", "Juan Perez"], :Alumnos =>[{:Nombre=>"Pedro",:Calificacion=>0.0}] },
+                  :BasesDeDatos=>{:Maestros=>["Olivia Martinez"], :Alumnos =>[{:Nombre=>"Juan",:Calificacion=>0.0}] }}
         nil
     end
     def ver_materias!
@@ -20,7 +20,17 @@ class Escuela
         end 
         almacen_maestros.each {|maestro| puts maestro}
     end
+    def ver_alumnos!
+        almacen_alumnos = []
+        @control.each_key do |materia|
+            @control[materia][:Alumnos].each do |alumno|
+                almacen_alumnos << alumno[:Nombre] unless almacen_alumnos.include? alumno[:Nombre] 
+            end
+        end 
+        almacen_alumnos.each {|alumno| puts alumno }
+    end
 end
 escuela = Escuela.new
 escuela.ver_materias!
 escuela.ver_maestros!
+escuela.ver_alumnos!
