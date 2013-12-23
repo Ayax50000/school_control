932c9e441f008b16a985123ffc0c7e035d5bddfd
Ayax50000 <fjlsoto@gmail.com>
2013-12-22 20:49:01 -0600

    method to see subjects done

--- SchoolControl.rb
+++ SchoolControl.rb
@@ -1,12 +1,16 @@
-class School
+class Escuela
     def initialize
         @control ={:Fisica=> {:Maestros=>["Juan Carlos Hernandez","Juan Perez"], :Alumnos=>[{:Alumno=>"",:Calificacion=>0.0}] },
                    :Matematicas=>{:Maestros=>["Olivia Martinez", "Juan Perez"], :Alumnos =>[{:Alumno=>"",:Calificacion=>0.0}] },
                   :BasesDeDatos=>{:Maestros=>["Olivia Martinez"], :Alumnos =>[{:Alumno=>"",:Calificacion=>0.0}] }}
+        nil
     end
-    def ver_materias
-        @control.each_key do |llave|
-        puts "los datos que ahy son #{llave}"
+    def ver_materias!
+        @control.each_key do |materia|
+        puts "#{materia}"
         end
+        nil
     end
 end
+escuela = Escuela.new
+escuela.ver_materias!
